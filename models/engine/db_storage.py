d3ff30f7cbace2c20d5f032950da553f2e5461ec
GetacherD <getacher.d@gmail.com>
2022-09-13 18:23:49 +0300

    update storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import scoped_session
 from models.base_model import Base
 from models.state import State
 from models.city import City
+from models.user import User
 
 
 class DBStorage:
