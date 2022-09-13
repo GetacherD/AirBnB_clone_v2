09a7caf6e45a5fd8db7566e4edddace7a2a4961d
GetacherD <getacher.d@gmail.com>
2022-09-13 18:44:58 +0300

    update rs

--- models/user.py
+++ models/user.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 from models.base_model import Base
-from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -14,4 +13,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete")
+    places = relationship("Place", backref="user", cascade="delete")
