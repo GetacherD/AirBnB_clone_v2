e588fdf29e4bc9c8e3fc80489e52e63c6a3468d5
GetacherD <getacher.d@gmail.com>
2022-09-29 14:04:26 +0300

    first

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,6 +5,7 @@ Generate .tgz files
 from os import path
 from datetime import datetime as dt
 from fabric.api import local, put, run, env
+from fabric.context_managers import lcd
 
 
 env.user = "ubuntu"
@@ -20,7 +21,7 @@ def do_pack():
     p = "versions/web_static_{}{}{}{}{}{}.tgz".format(
         archived.year, archived.month, archived.day,
         archived.hour, archived.minute, archived.second)
-    if local("tar -cvzf {} web_static".format(
+    if local("tar -cvzf {} web_static/".format(
             p)).failed is True:
         return None
     return p
@@ -29,24 +30,35 @@ def do_pack():
 def do_deploy(archive_path):
     """ Deploy files to servers"""
     print("archive_path")
-    if path.isfile("versions/{}".format(archive_path)) is False:
+    if path.isfile("{}".format(archive_path)) is False:
         return False
-    result = put(
-        "versions/{}".format(archive_path), "/tmp/{}".format(archive_path))
+    result = put("{}".format(
+        archive_path), "/tmp/{}".format(
+            archive_path.split("/")[1]))
     if result.failed is True:
         return False
-    extract_path = archive_path.split(".")[0]
+    extract_path = archive_path.split("/")[1].split(".")[0]
+    if run("rm -rf /data/web_static/releases/{}/".format(
+            extract_path)).failed is True:
+        return False
     if run(
             "mkdir -p /data/web_static/releases/{}".format(
                 extract_path)).failed is True:
         return False
-    if run("tar -xf /tmp/{} -C /data/web_static/releases/{}".format(
-            archive_path, extract_path)).failed is True:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+            archive_path.split("/")[1], extract_path)).failed is True:
         return False
-    if run("rm -rf /tmp/{}".format(archive_path)).failed is True:
+    if run("rm -rf /tmp/{}".format(archive_path.split("/")[1])).failed is True:
         return False
+    part1 = "mv /data/web_static/releases/{}/web_static/*".format(extract_path)
+    part2 = "/data/web_static/releases/{}/".format(extract_path)
+    mv = "{} {}".format(part1, part2)
+    if run(mv).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        pass
     if run(
-        "rm /data/web_static/current && ln -sf /data/web_static/releases/{}"
+        "ln -sf /data/web_static/releases/{}/ /data/web_static/current"
             .format(extract_path)).failed is True:
         return False
     return True
