bb8d5dda9da10742c3d8f2f56eb786b6dc2cefc2
GetacherD <getacher.d@gmail.com>
2022-10-16 21:48:54 +0300

    update console

--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,57 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import uuid
+"""
+Base Model to be inherited
+"""
 from os import getenv
+import uuid
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, DateTime
 import models
 
-if getenv("HBNB_TYPE_STORAGE", None) == "db":
+Base = object
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from sqlalchemy.orm import declarative_base
     Base = declarative_base()
-else:
-    Base = object
 
 
-class BaseModel:
-    """A base class for all hbnb models"""
+class BaseModel(Base):
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        id = Column(String(60), primary_key=True, nullable=False, unique=True)
-        created_at = Column(
-            DateTime, nullable=False, default=datetime.utcnow())
-        updated_at = Column(
-            DateTime, nullable=False, default=datetime.utcnow())
-
+    """ Base Model Representation """
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+
+        """ Initialization with or with out kwargs
+        kwargs assumed to contain isoformatted datetime object
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key in ["created_at", "updated_at"]:
-                    if isinstance(value, str):
-                        value = datetime.fromisoformat(value)
-                if key != "__class__":
-                    setattr(self, key, value)
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value
+            if self.__dict__.get("__class__", None):
+                del self.__dict__["__class__"]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = str(type(self).__name__)
-        d = self.__dict__.copy()
-        d.pop("_sa_instance_state", None)
-        return '[{}] ({}) {}'.format(cls, self.id, d)
+
+        """ String Representation """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+
+        """ save instance to file"""
         self.updated_at = datetime.utcnow()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        if my_dict.get("_sa_instance_state", None):
-            del my_dict["_sa_instance_state"]
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        my_dict["created_at"] = self.created_at.isoformat()
-        return my_dict
 
-    def delete(self):
-        """ Delete self from database"""
-        models.storage.delete(self)
+        """ Return Dict Representation """
+        dic = self.__dict__.copy()
+        dic["updated_at"] = dic["updated_at"].isoformat()
+        dic["created_at"] = dic["created_at"].isoformat()
+        dic["__class__"] = self.__class__.__name__
+        return dic
