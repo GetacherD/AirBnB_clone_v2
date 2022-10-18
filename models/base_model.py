48b7f96a63c2ce49da14437c7fcb7a414df90144
GetacherD <getacher.d@gmail.com>
2022-10-18 09:59:01 +0300

    update save

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             if kwargs.get("updated_at"):
                 kwargs['updated_at'] = datetime.strptime(
@@ -30,7 +31,7 @@ class BaseModel:
             if not kwargs.get("id"):
                 kwargs["id"] = str(uuid.uuid4())
             self.__dict__.update(kwargs)
-        models.storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
