6ca2dd98438c5632e6a06f53d6a3deb90a7d03a4
GetacherD <getacher.d@gmail.com>
2022-10-20 21:20:52 +0300

    no call to scooped session

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -81,8 +81,8 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """ close session """
-        self.__session.close()
+        self.__session.remove()
