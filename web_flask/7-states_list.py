8d17cd8128a54a0f32d7bd412e46325211968451
GetacherD <getacher.d@gmail.com>
2022-10-20 21:30:11 +0300

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,68 +10,17 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(self):
-    """ close connection"""
-    storage.close()
-
-
-@app.route("/", strict_slashes=False)
-def index():
-    """ Index Page """
-    return "Hello HBNB!"
-
-
 @app.route("/states_list", strict_slashes=False)
 def get_state():
     """ get all states_list """
-    state = storage.all('State')
+    state = sorted(list(storage.all('State')), key=lambda x: x.name)
     return render_template("7-states_list.html", state=state)
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb_home():
-    """ Display hbnb home"""
-    return "HBNB"
-
-
-@app.route("/c/<string:text>", strict_slashes=False)
-def get_text(text):
-    """ get url params"""
-    if text:
-        value = str(text).replace("_", " ")
-        return "C {}".format(value)
-
-
-@app.route("/python/<text>", strict_slashes=False)
-@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
-def get_python(text):
-    """ python smart route"""
-    value = "is cool"
-    if text:
-        value = text.replace("_", " ")
-    return "Python {}".format(value)
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def is_num(n):
-    """check if number"""
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def num_template(n):
-    """ number template example"""
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def odd_or_even(n):
-    """ check odd or even"""
-    val = "odd"
-    if n % 2 == 0:
-        val = "even"
-    return render_template("6-number_odd_or_even.html", n=n, val=val)
+@app.teardown_appcontext
+def teardown(exc):
+    """ close connection"""
+    storage.close()
 
 
 if __name__ == "__main__":
