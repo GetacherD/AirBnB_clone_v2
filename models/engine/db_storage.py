236b4c0f580a48688cae75a3a281502d048e01bc
GetacherD <getacher.d@gmail.com>
2022-10-21 15:44:58 +0300

    update for scooped session

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -45,7 +45,7 @@ class DBStorage:
         new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
+                objs = self.__session().query(classes[clss]).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
@@ -53,23 +53,23 @@ class DBStorage:
 
     def new(self, obj):
         """add the object to the current database session"""
-        self.__session.add(obj)
+        self.__session().add(obj)
 
     def save(self):
         """commit all changes of the current database session"""
-        self.__session.commit()
+        self.__session().commit()
 
     def delete(self, obj=None):
         """delete from the current database session obj if not None"""
         if obj is not None:
-            self.__session.delete(obj)
+            self.__session().delete(obj)
 
     def reload(self):
         """reloads data from the database"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session
+        session = scoped_session(sess_factory)
+        self.__session = session
 
     def close(self):
         """call remove() method on the private session attribute"""
